3fae80f9c49a42b006970d9262d8ec7f95fb07ee
songyang <guosongyang@52yunxi.cn>
2019-05-31 11:40:19 +0800

    Update code1.vue

--- src/views/technology/code/code1.vue
+++ src/views/technology/code/code1.vue
@@ -1,8 +1,12 @@
 <template>
     <div class="margin40">
-        <h1 class="bgBlue">标题</h1>
+        <h1 class="bgBlue">我理解的vue的路由</h1>
         <div class="mainText">
-            内容
+            vue的路由是什么。
+        </div>
+        <div class="mainText">
+            vue的官方路由依赖是vue router 用这个可以很简单的构建出单页面应用（spa）<br />
+            步骤一：将vue-router 引入，然后将组件components 映射到路由。其实就是使用"<router-view>" 这个标签进行路由的展示<br />
         </div>
     </div>
 </template>
