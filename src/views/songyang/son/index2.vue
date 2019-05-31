8cad3d329f515d0279488195595c9ec9c86f38fc
songyang <guosongyang@52yunxi.cn>
2019-05-31 11:26:55 +0800

    1

--- src/views/songyang/son/index2.vue
+++ src/views/songyang/son/index2.vue
@@ -1,6 +1,20 @@
 <template>
-    <div class="dashboard-container">
-        <div class="dashboard-text">2222222</div>
+    <div class="margin40">
+        <h1 class="bgBlue">最近比较在意的10件事情是什么。</h1>
+        <div class="mainText">
+            1 租房子
+            2 甲沟炎
+            3 如何入门漫画
+            4 如何在增加前端开发的经验
+            5 如何让钱包鼓一些（没钱去租房子和看脚了都。）
+            6 梦幻的钱怎么拿回来。（直接卖号的话亏很多。）
+        </div>
+        <div class="mainText">
+            刚需是：1 租房子 2 甲沟炎
+        </div>
+        <div class="mainText">
+            那就先不管其他的事情，先找好房子，和认真预防甲沟炎。
+        </div>
     </div>
 </template>
 
@@ -13,13 +27,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.dashboard {
-    &-container {
-        margin: 30px;
-    }
-    &-text {
-        font-size: 30px;
-        line-height: 46px;
-    }
-}
+
 </style>
